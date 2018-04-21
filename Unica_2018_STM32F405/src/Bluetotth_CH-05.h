2c700aa019d5fef5374a39adfd98e7e4765d7c90
Korr237i <qwerty2012i@yandex.ru>
2018-04-21 20:58:33 +0300

    Делаем DMA. Не работает

---
+++ Unica_2018_STM32F405/src/Bluetotth_CH-05.h
@@ -0,0 +1,59 @@
+/*
+ * Bluetotth_CH-05.h
+ *
+ *  Created on: 7 апр. 2018 г.
+ *      Author: developer
+ */
+
+#ifndef BLUETOTTH_CH_05_H_
+#define BLUETOTTH_CH_05_H_
+
+#define AT_RESET
+#define AT_VERSION
+#define AT_ORGL
+#define AT_ADDR
+
+#define AT_NAME
+#define AT_NAME_Q
+
+#define AT_RNAME
+
+#define AT_ROLE
+#define AT_ROLE_Q
+
+#define AT_CLASS
+#define AT_CLASS_Q
+
+#define AT_IAC
+#define AT_IAC_Q
+
+#define AT_INQM
+#define AT_INQM_Q
+
+#define AT_PSWD
+#define AT_PSWD
+
+#define AT_UART
+#define AT_UART_Q
+
+#define AT_CMODE
+#define AT_CMODE_Q
+
+#define AT_BIND
+#define AT_BIND_Q
+
+#define AT_POLAR
+#define AT_POLAR_Q
+
+#define AT_PIO
+
+#define AT_IPSCAN
+#define AT_IPSCAN_Q
+
+#define AT_SNIFF
+#define AT_SNIFF_Q
+
+#define AT_SENM
+#define AT_SENM_Q
+
+#endif /* BLUETOTTH_CH_05_H_ */

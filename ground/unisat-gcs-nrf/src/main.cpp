9c88b75d32e1cb641e0a41d6fc175716e1229b6e
dll31 <danila.litvinov31@yandex.ru>
2018-05-04 19:47:11 +0300

    Очепятки и устаревшее

--- ground/unisat-gcs-nrf/src/main.cpp
+++ ground/unisat-gcs-nrf/src/main.cpp
@@ -34,19 +34,20 @@ int main(int argc, char ** argv)
 	using namespace boost;
 	using udp = boost::asio::ip::udp;
 
-	RF24 radio1(24, BCM2835_SPI_CS1); //, BCM2835_SPI_SPEED_8MHZ);
+	//RF24 radio1(22, BCM2835_SPI_CS0); //, BCM2835_SPI_SPEED_8MHZ);
+	RF24 radio(24, BCM2835_SPI_CS1); //, BCM2835_SPI_SPEED_8MHZ);
 
-	radio1.begin();
+	radio.begin();
 
-	radio1.setChannel(NRF_CHANNEL);
-	radio1.setAddressWidth(5);
-	radio1.setDataRate(RF24_1MBPS);
-	radio1.setCRCLength(RF24_CRC_16);
-	radio1.enableDynamicPayloads();
-	radio1.enableAckPayload();
-	radio1.openWritingPipe(0xE7E7E7E7E7);
-	radio1.openReadingPipe(1, 0xE7E7E7E7E7);
-	radio1.startListening();
+	radio.setChannel(NRF_CHANNEL);
+	radio.setAddressWidth(5);
+	radio.setDataRate(RF24_1MBPS);
+	radio.setCRCLength(RF24_CRC_16);
+	radio.enableDynamicPayloads();
+	radio.enableAckPayload();
+	radio.openWritingPipe(0xE7E7E7E7E7);
+	radio.openReadingPipe(1, 0xE7E7E7E7E7);
+	radio.startListening();
 
 	uint8_t rx_pipeno;
 	uint8_t buffer[32];
@@ -59,12 +60,12 @@ int main(int argc, char ** argv)
 		udp::endpoint target_endpoint = *resolver.resolve(query);
 		socket.open(target_endpoint.protocol());
 
-		radio1.printDetails();
+		radio.printDetails();
 
 		while(true) {
-			if( radio1.available(&rx_pipeno) ) {
-				size_t len = radio1.getDynamicPayloadSize();
-				radio1.read( buffer, len );
+			if( radio.available(&rx_pipeno) ) {
+				size_t len = radio.getDynamicPayloadSize();
+				radio.read( buffer, len );
 				std::cout << "Got packet! " << len << std::endl;
 
 				socket.send_to(asio::buffer(buffer, len), target_endpoint);
